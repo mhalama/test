5c5a226e2f4349bc3885089a3f54918e354dbf23
Martin Halama <martin@halama.eu>
2023-03-22 12:15:13 +0100

    x

--- boot.py
+++ boot.py
@@ -1,8 +1,8 @@
 import network
 
 with open("wifi") as f:
-    ssid = f.read()
-    pwd = f.read()
+    ssid = f.readline().rstrip()
+    pwd = f.readline().rstrip()
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
